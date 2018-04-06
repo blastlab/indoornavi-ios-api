a433bf6ce585ba08a4337cc29a7112a1bf6aa386
Michał Pastwa <mpastwa@blastlab.co>
2018-04-06 16:42:27 +0200

    Usunięcie dodawania do biblioteki niepotrzebnych plików źródłowych. NAVI-213

--- IndoorNavi/IndoorNavi/IndoorNavi.swift
+++ IndoorNavi/IndoorNavi/IndoorNavi.swift
@@ -38,7 +38,7 @@ public class IndoorNavi: UIView, WKUIDelegate, WKNavigationDelegate, WKScriptMes
     }
     
     required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     private func initInJavaScript() {
