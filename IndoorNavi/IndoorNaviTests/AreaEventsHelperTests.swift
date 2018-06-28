efbcd090144ff52ae93629bf75bbe73f4d2306d2
Michał Pastwa <mpastwa@blastlab.co>
2018-06-28 14:07:18 +0200

    Testy ColorHelper, testy AreaEventsHelper. NAVI-309

---
+++ IndoorNavi/IndoorNaviTests/AreaEventsHelperTests.swift
@@ -0,0 +1,22 @@
+//
+//  AreaEventsHelperTests.swift
+//  IndoorNaviTests
+//
+//  Created by Michał Pastwa on 25.06.2018.
+//  Copyright © 2018 BlastLab. All rights reserved.
+//
+
+import XCTest
+@testable import IndoorNavi
+
+class AreaEventsHelperTests: XCTestCase {
+    
+    let areaEvents = [AreaEvent(tagID: 18, date: Date(timeIntervalSince1970: 1428200000), areaID: 3, areaName: "Hala 1", mode: .onEnter), AreaEvent(tagID: 10, date: Date(timeIntervalSince1970: 1428100000), areaID: 6, areaName: "Hala 2", mode: .onLeave)]
+    let areaEventsDictionary: [[String: Any]] = [["date": Date(timeIntervalSince1970: 1428200000), "tagId": 18, "areaId": 3, "areaName": "Hala 1", "mode": "ON_ENTER"], ["date": Date(timeIntervalSince1970: 1428100000), "tagId": 10, "areaId": 6, "areaName": "Hala 2", "mode": "ON_LEAVE"]]
+    
+    func testExample() {
+        let areaEventsArray = AreaEventsHelper.areaEvents(fromJSONObject: areaEventsDictionary)
+        XCTAssertEqual(areaEventsArray, areaEvents)
+    }
+}
+//"2015-04-03 00:00:00 +0000"
--- IndoorNavi/IndoorNaviTests/ColorHelperTests.swift
+++ IndoorNavi/IndoorNaviTests/ColorHelperTests.swift
@@ -13,14 +13,6 @@ class ColorHelperTests: XCTestCase {
     
     let (red, green, blue): (CGFloat, CGFloat, CGFloat) = (0.5, 0.8, 0.2)
     let colorString = "rgb(127,204,51)"
-    
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testColorStringFromComponents() {
         let colorStringFromComponents = ColorHelper.colorStringFromColorComponents(red: red, green: green, blue: blue)
