551912bb678d9e9247e37fbb552ea62948f44d58
Michał Pastwa <mpastwa@blastlab.co>
2018-07-07 11:50:28 +0200

    Dostosowanie MapHelper do Objective-C. NAVI-320

--- IndoorNavi/IndoorNavi/Helpers/MapHelper.swift
+++ IndoorNavi/IndoorNavi/Helpers/MapHelper.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 BlastLab. All rights reserved.
 //
 
-public class MapHelper {
+public class MapHelper: NSObject {
     
     /// Converts pixel point to the point corresponding to the actual location in proper unit.
     ///
@@ -21,6 +21,14 @@ public class MapHelper {
         return point
     }
     
+    @available(swift, obsoleted: 1.0)
+    @objc public static func realCoordinates(fromPixel pixel: INPoint, scale: ObjCScale) -> INPoint {
+        let x = Int32(Double(pixel.x) * scale.centimetresPerPixel)
+        let y = Int32(Double(pixel.y) * scale.centimetresPerPixel)
+        let point = INPoint(x: x, y: y)
+        return point
+    }
+    
     /// Converts point given in real dimension unit to the pixel point
     ///
     /// - Parameters:
@@ -28,6 +36,14 @@ public class MapHelper {
     ///   - scale: `Scale` of the map.
     /// - Returns: `INPoint` describing location of the pixel.
     public static func pixel(fromReaCoodinates realCoordinates: INPoint, scale: Scale) -> INPoint {
+        let x = Int32(Double(realCoordinates.x) / scale.centimetresPerPixel)
+        let y = Int32(Double(realCoordinates.y) / scale.centimetresPerPixel)
+        let point = INPoint(x: x, y: y)
+        return point
+    }
+    
+    @available(swift, obsoleted: 1.0)
+    @objc public static func pixel(fromReaCoodinates realCoordinates: INPoint, scale: ObjCScale) -> INPoint {
         let x = Int32(Double(realCoordinates.x) / scale.centimetresPerPixel)
         let y = Int32(Double(realCoordinates.y) / scale.centimetresPerPixel)
         let point = INPoint(x: x, y: y)
