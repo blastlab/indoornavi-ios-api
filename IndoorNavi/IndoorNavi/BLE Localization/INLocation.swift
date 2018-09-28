667dd1e29bababd62457373a0d30e34fb0aa85e7
Michał Pastwa <mpastwa@blastlab.co>
2018-09-28 11:21:29 +0200

    Dokumentacja. NAVI-387

--- IndoorNavi/IndoorNavi/BLE Localization/INLocation.swift
+++ IndoorNavi/IndoorNavi/BLE Localization/INLocation.swift
@@ -6,14 +6,18 @@
 //  Copyright © 2018 BlastLab. All rights reserved.
 //
 
+/// The XY coordinates representing current location.
 public struct INLocation {
+    /// The x-coordinate.
     public var x: Double
+    /// The y-coordinate.
     public var y: Double
     
     public static func == (lhs: INLocation, rhs: INLocation) -> Bool {
         return lhs.x == rhs.x && lhs.y == rhs.y
     }
     
+    /// The point with location (0,0).
     public static var zero: INLocation {
         return INLocation(x: 0, y: 0)
     }
