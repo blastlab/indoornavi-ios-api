50f17953dd49496e6618fef118a2800e8e7f5d38
Michał Pastwa <mpastwa@blastlab.co>
2019-03-26 17:23:26 +0100

    Poprawki w testach, konwersja do Swift 4.2. NAVI-551

--- IndoorNavi/IndoorNaviTests/HelpersTests/ComplexHelperTests.swift
+++ IndoorNavi/IndoorNaviTests/HelpersTests/ComplexHelperTests.swift
@@ -9,15 +9,15 @@
 import XCTest
 @testable import IndoorNavi
 
-let complexes1 = [Complex(identifier: 1, name: "Complex 1", buildings: buildingsC1), Complex(identifier: 2, name: "Complex 2", buildings: buildingsC2)]
-let complexes2 = [Complex(identifier: 1, name: "Kompleks 1", buildings: buildingsC1), Complex(identifier: 2, name: "Kompleks 2", buildings: buildingsC2)]
+let complexes1 = [Complex(id: 1, name: "Complex 1", buildings: buildingsC1), Complex(id: 2, name: "Complex 2", buildings: buildingsC2)]
+let complexes2 = [Complex(id: 1, name: "Kompleks 1", buildings: buildingsC1), Complex(id: 2, name: "Kompleks 2", buildings: buildingsC2)]
 
-let buildingsC1 = [Building(identifier: 1, name: "A", floors: floorsA), Building(identifier: 2, name: "B", floors: floorsB), Building(identifier: 3, name: "C", floors: floorsC)]
-let buildingsC2 = [Building(identifier: 1, name: "D", floors: floorsA), Building(identifier: 2, name: "E", floors: floorsB), Building(identifier: 3, name: "F", floors: floorsC)]
+let buildingsC1 = [Building(id: 1, name: "A", floors: floorsA), Building(id: 2, name: "B", floors: floorsB), Building(id: 3, name: "C", floors: floorsC)]
+let buildingsC2 = [Building(id: 1, name: "D", floors: floorsA), Building(id: 2, name: "E", floors: floorsB), Building(id: 3, name: "F", floors: floorsC)]
 
-let floorsA = [Floor(identifier: 1, name: "Parter", level: 0), Floor(identifier: 2, name: "I piętro", level: 1), Floor(identifier: 3, name: "II piętro", level: 2)]
-let floorsB = [Floor(identifier: 4, name: "Garaż", level: 0), Floor(identifier: 5, name: "I p", level: 1), Floor(identifier: 6, name: "II p", level: 2)]
-let floorsC = [Floor(identifier: 7, name: "Ground", level: 0), Floor(identifier: 8, name: "I level", level: 1), Floor(identifier: 9, name: "II level", level: 2)]
+let floorsA = [Floor(id: 1, name: "Parter", level: 0), Floor(id: 2, name: "I piętro", level: 1), Floor(id: 3, name: "II piętro", level: 2)]
+let floorsB = [Floor(id: 4, name: "Garaż", level: 0), Floor(id: 5, name: "I p", level: 1), Floor(id: 6, name: "II p", level: 2)]
+let floorsC = [Floor(id: 7, name: "Ground", level: 0), Floor(id: 8, name: "I level", level: 1), Floor(id: 9, name: "II level", level: 2)]
 
 let complexes1Dictionary: Any = ["complexes": [["id": 1, "name": "Complex 1", "buildings": buildingsC1Dictionary], ["id": 2, "name": "Complex 2", "buildings": buildingsC2Dictionary]]]
 let complexes2Dictionary: Any = ["complexes": [["id": 1, "name": "Kompleks 1", "buildings": buildingsC1Dictionary], ["id": 2, "name": "Kompleks 2", "buildings": buildingsC2Dictionary]]]
@@ -32,25 +32,68 @@ let floorsCDictionary: Any = [["id": 7, "level": 0, "name": "Ground"], ["id": 8,
 class ComplexHelperTests: XCTestCase {
     
     func testComplexesFromJSON() {
-        let complexesArray1 = ComplexHelper.complexes(fromJSONObject: complexes1Dictionary)
-        let complexesArray2 = ComplexHelper.complexes(fromJSONObject: complexes2Dictionary)
+        
+        let data1 = try? JSONSerialization.data(withJSONObject: complexes1Dictionary, options: .prettyPrinted)
+        let data2 = try? JSONSerialization.data(withJSONObject: complexes2Dictionary, options: .prettyPrinted)
+        
+        guard let complexesData1 = data1, let complexesData2 = data2 else {
+            XCTFail()
+            return
+        }
+        
+        let array1 = try? JSONDecoder().decode([Complex].self, from: complexesData1)
+        let array2 = try? JSONDecoder().decode([Complex].self, from: complexesData2)
+        
+        guard let complexesArray1 = array1, let complexesArray2 = array2 else {
+            XCTFail()
+            return
+        }
         
         XCTAssertEqual(complexesArray1, complexes1)
         XCTAssertEqual(complexesArray2, complexes2)
     }
     
     func testBuildingsFromJSON() {
-        let buildingsArrayC1 = ComplexHelper.buildings(fromJSONObject: buildingsC1Dictionary)
-        let buildingsArrayC2 = ComplexHelper.buildings(fromJSONObject: buildingsC2Dictionary)
+        
+        let data1 = try? JSONSerialization.data(withJSONObject: buildingsC1Dictionary, options: .prettyPrinted)
+        let data2 = try? JSONSerialization.data(withJSONObject: buildingsC2Dictionary, options: .prettyPrinted)
+        
+        guard let buildingsData1 = data1, let buildingsData2 = data2 else {
+            XCTFail()
+            return
+        }
+        
+        let array1 = try? JSONDecoder().decode([Building].self, from: buildingsData1)
+        let array2 = try? JSONDecoder().decode([Building].self, from: buildingsData2)
+        
+        guard let buildingsArrayC1 = array1, let buildingsArrayC2 = array2 else {
+            XCTFail()
+            return
+        }
         
         XCTAssertEqual(buildingsArrayC1, buildingsC1)
         XCTAssertEqual(buildingsArrayC2, buildingsC2)
     }
     
     func testFloorsFromJSON() {
-        let floorsArrayA = ComplexHelper.floors(fromJSONObject: floorsADictionary)
-        let floorsArrayB = ComplexHelper.floors(fromJSONObject: floorsBDictionary)
-        let floorsArrayC = ComplexHelper.floors(fromJSONObject: floorsCDictionary)
+        
+        let data1 = try? JSONSerialization.data(withJSONObject: floorsADictionary, options: .prettyPrinted)
+        let data2 = try? JSONSerialization.data(withJSONObject: floorsBDictionary, options: .prettyPrinted)
+        let data3 = try? JSONSerialization.data(withJSONObject: floorsCDictionary, options: .prettyPrinted)
+        
+        guard let floorsData1 = data1, let floorsData2 = data2, let floorsData3 = data3 else {
+            XCTFail()
+            return
+        }
+        
+        let array1 = try? JSONDecoder().decode([Floor].self, from: floorsData1)
+        let array2 = try? JSONDecoder().decode([Floor].self, from: floorsData2)
+        let array3 = try? JSONDecoder().decode([Floor].self, from: floorsData3)
+        
+        guard let floorsArrayA = array1, let floorsArrayB = array2, let floorsArrayC = array3 else {
+            XCTFail()
+            return
+        }
         
         XCTAssertEqual(floorsArrayA, floorsA)
         XCTAssertEqual(floorsArrayB, floorsB)
